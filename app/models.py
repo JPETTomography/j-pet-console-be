ad8b99564c06ca4ac4b1efc5f3a739bb9c3f7449
TheSobkiewicz <57150518+TheSobkiewicz@users.noreply.github.com>
2024-10-15 15:29:26 +0200

    Empty line

--- app/models.py
+++ app/models.py
@@ -9,4 +9,3 @@ class User(Base):
     email = Column(String, unique=True, nullable = False)
     password = Column(String, nullable = False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
-    
