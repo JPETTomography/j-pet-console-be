4e074a8574d8bc9d814f1f77779994b0e5673c89
Jakub Perlak <j.perlak@cyfronet.pl>
2024-11-12 17:46:53 +0100

    Join users name to read_experiments query

--- backend/routers/experiments.py
+++ backend/routers/experiments.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectinload, load_only
 import database.models as models
 from database.database import get_session_local
 from backend.utills.utills import generate_fake_experiment
@@ -8,7 +8,7 @@ router = APIRouter()
 
 @router.get("/")
 def read_experiments(db: Session = Depends(get_session_local)):
-    return db.query(models.Experiment).all()
+    return db.query(models.Experiment).options(selectinload(models.Experiment.owner).load_only(models.User.name)).all()
 
 @router.get("/{id}")
 def read_experiment(id: str, db: Session = Depends(get_session_local)):
