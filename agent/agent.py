65cf3f3a0716859eaebc2848634a3fc3d544750a
mwmajewsk <5279578+mmajewsk@users.noreply.github.com>
2024-11-20 13:32:47 +0100

    base changes

--- agent/agent.py
+++ agent/agent.py
@@ -3,19 +3,24 @@ import logging
 import socket
 import json
 import pika
+import yaml
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
 # HOSTNAME = 'localhost'
-HOST = 'agent'
+HOST = "agent"
 PORT = 12345
 TOPIC = "root_json"
 
 
 rabbitmq_host = "rabbitmq"
-credentials = pika.PlainCredentials('user', 'password')
+credentials = pika.PlainCredentials("user", "password")
 parameters = pika.ConnectionParameters(
     rabbitmq_host,  # replace with RabbitMQ server IP if not local
-    5672,         # default RabbitMQ port
-    '/',
-    credentials
+    5672,  # default RabbitMQ port
+    "/",
+    credentials,
 )
 
 
@@ -25,34 +30,32 @@ def send_message(topic, message):
     channel = connection.channel()
 
     # Declare a queue
-    channel.queue_declare(queue='task_queue', durable=True)
+    channel.queue_declare(queue="task_queue", durable=True)
 
     # Publish a message
     channel.basic_publish(
-        exchange='',
-        routing_key='worker_topic',
+        exchange="",
+        routing_key="worker_topic",
         body=message,
         properties=pika.BasicProperties(
             delivery_mode=2,  # Make message persistent
-        ))
+        ),
+    )
 
     print(f"Sent: {message}")
     connection.close()
 
 
-def consume_messages():
-    # Connect to RabbitMQ
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
+# def consume_messages():
+#     connection = pika.BlockingConnection(parameters)
+#     channel = connection.channel()
 
-    # Declare the queue (make sure it exists)
-    channel.queue_declare(queue='agent_topic', durable=True)
-    # Set up consumer
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue='agent_topic', on_message_callback=callback)
+#     channel.queue_declare(queue='agent_topic', durable=True)
+#     channel.basic_qos(prefetch_count=1)
+#     channel.basic_consume(queue='agent_topic', on_message_callback=callback)
 
-    print("Waiting for messages...")
-    channel.start_consuming()
+#     print("Waiting for messages...")
+#     channel.start_consuming()
 
 
 def send_data(json_data, host=HOST, port=PORT):
@@ -65,36 +68,60 @@ def send_data(json_data, host=HOST, port=PORT):
         f.write(f"Server listening on {host}:{port}...")
 
     connection_info = {"ip": HOST, "port": PORT}
-    encoded_info = json.dumps(connection_info).encode('utf-8')
+    encoded_info = json.dumps(connection_info).encode("utf-8")
     print("Sending connection info to Rabbit...")
     send_message("worker_topic", encoded_info)
     while True:
         connection, client_address = server_socket.accept()
         try:
             print(f"Connection established with {client_address}")
-            encoded_data = json.dumps(json_data).encode('utf-8')
+            encoded_data = json.dumps(json_data).encode("utf-8")
             connection.sendall(encoded_data)
         finally:
             connection.close()
             print(f"Connection with {client_address} closed.")
 
 
-def callback(ch, method, properties, body):
-    print(f"Received: {body.decode()}")
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-    hist_def = "examplary_data/histo_description.json"
-    root_file_path = "examplary_data/2024_02_14_14_57_dabc_24043183007.root"
+def process_file(root_file_path, hist_def):
+    # hist_def = "examplary_data/histo_description.json"
+    # root_file_path = "examplary_data/2024_02_14_14_57_dabc_24043183007.root"
     print("reading_data")
     root_json_data = root_file_to_json(hist_def, root_file_path)
+    send_data(root_json_data)
 
-    task_info = json.loads(body.decode())
-    match task_info["task"]:
-        case "add_random_test_data":
-            print("data read finished")
-            send_data(root_json_data)
-        case _:
-            raise ValueError(f"Unknown task: {task_info['task']}")
+
+def read_config(filepath):
+    with open(filepath) as f:
+        config = yaml.safe_load(f)
+    return config
+
+
+class NewFileHandler(FileSystemEventHandler):
+    def __init__(self, agent_config):
+        self.agent_config = agent_config
+
+    def on_created(self, event):
+        if not event.is_directory:
+            print(f"New file detected: {event.src_path}")
+            self.on_new_file_callback(
+                root_file_path=event.src_path,
+                hist_def=self.agent_config.hist_def
+            )
 
 
 if __name__ == "__main__":
-    consume_messages()
+    # consume_messages()
+    config = read_config("./examplary_config.yaml")
+    event_handler = NewFileHandler(agent_config=config)
+    observer = Observer()
+    path_to_watch = config["detector"]
+    observer.schedule(event_handler, path=path_to_watch, recursive=False)
+    observer.start()
+    print(f"Watching folder: {path_to_watch}")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
