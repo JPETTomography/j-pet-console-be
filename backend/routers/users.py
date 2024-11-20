50634a851e707d220bc77510f463e7389280fca7
Jakub Perlak <j.perlak@cyfronet.pl>
2024-11-20 19:19:47 +0100

    Improve code formatting

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 import database.models as models
 from database.database import get_session_local
 from backend.utills.utills import generate_fake_user
+
 router = APIRouter()
 
 @router.get("/")
