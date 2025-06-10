99fcd62ed5c1e986de457f13ac51fd29414c3611
Tomasz Sobkiewicz <tomasz.sobkiewicz@swmansion.com>
2025-06-10 11:27:42 +0200

    Added photos

--- backend/routers/detectors.py
+++ backend/routers/detectors.py
@@ -54,18 +54,30 @@ def edit_detector(
             status_code=500, detail=f"Failed to update detector: {str(e)}"
         )
 
+
 generator = faker.Faker()
-def generate_fake_detector(db: Session=None):
+
+
+def generate_fake_detector(db: Session = None):
     i = 0
     while True:
-        ending = i%10
+        ending = i % 10
         yield dict(
             name=generator.catch_phrase(),
             description=generator.text(max_nb_chars=200),
-            status=random.choice(["online", "offline", "damaged", "in-repair", "commissioned", "decommissioned"]),
-            agent_code=f"550e8400-e29b-41d4-a716-44665544000{ending}"
+            status=random.choice(
+                [
+                    "online",
+                    "offline",
+                    "damaged",
+                    "in-repair",
+                    "commissioned",
+                    "decommissioned",
+                ]
+            ),
+            agent_code=f"550e8400-e29b-41d4-a716-44665544000{ending}",
         )
-        i+=1
+        i += 1
 
 
 @router.post("/create_sample_detectors/")
