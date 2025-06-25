68bd08633624c63aaf575c7569580a340e94371c
Tomasz Sobkiewicz <tomasz.sobkiewicz@swmansion.com>
2025-06-25 16:04:41 +0200

    Fixed origins

--- backend/main.py
+++ backend/main.py
@@ -82,7 +82,7 @@ app.include_router(
 origins = [os.getenv("CORS_ORIGIN", "http://localhost:3000")]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
